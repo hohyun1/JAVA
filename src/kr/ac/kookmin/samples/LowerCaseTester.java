a1772ba5bab5f4a7b86bd8cc2e4ecec4d512f57a
hohyun1 <105334941+hohyun1@users.noreply.github.com>
2022-09-22 12:55:13 +0900

    Add files via upload

---
+++ src/kr/ac/kookmin/samples/LowerCaseTester.java
@@ -0,0 +1,15 @@
+package kr.ac.kookmin.samples;
+
+public class LowerCaseTester {
+	public static void main(String[] args) {
+      String testString = "This is a Test";
+      String smallTestString = testString.toLowerCase();
+  
+      System.out.println(testString);
+      System.out.println(smallTestString);
+  
+      String bigTestString = smallTestString.toUpperCase();
+  
+      System.out.println(bigTestString);
+  }
+}
