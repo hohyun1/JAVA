ad0aaa67078c23264e122aebde767d63724c3c2b
hohyun1 <105334941+hohyun1@users.noreply.github.com>
2022-09-27 12:48:45 +0900

    lab04 실습과제

---
+++ samples/ArithmeticTester.java
@@ -0,0 +1,18 @@
+package kr.ac.kookmin.samples;
+
+public class ArithmeticTester 
+{
+	public static void main(String[] args) 
+       {
+
+		int age1 = 18;
+		int age2 = 35;
+		int age3 = 50;
+		int age4 = 44;
+		float page = age1+age2+age3+age4;
+		
+               double averageAge = (page)/4;
+		System.out.println(averageAge);
+				
+	}
+}
