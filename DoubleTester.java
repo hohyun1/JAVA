762edb6bbc4ae7e2307be155a4a3f3235f897f71
hohyun1 <105334941+hohyun1@users.noreply.github.com>
2022-09-27 12:48:18 +0900

    lab04 실습과제

---
+++ DoubleTester.java
@@ -0,0 +1,12 @@
+package kr.ac.kookmin.samples;
+
+public class DoubleTester 
+{
+	public static void main (String args[])
+	{
+		double probability = 8.70;
+		int percentage = (int) Math.round(100 * probability);
+		System.out.println(percentage);
+	}
+
+}
