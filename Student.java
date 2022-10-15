1d172aeea79d76763711c4fa842c52e136b8facd
hohyun1 <105334941+hohyun1@users.noreply.github.com>
2022-10-15 20:58:59 +0900

    객지프 7-3(2)

---
+++ Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+	private String names;
+	private int scores;
+	public Student(String name, int score) {
+		this.names=name;
+		this.scores=score;
+	}
+	public int score()
+	{
+		return scores;
+	}
+	public String name()
+	{
+		return names;
+	}
+}
